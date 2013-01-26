39f28177c091fd4c79ee41266989007dbc0cf3e3
Joni Korpi <me@jonikorpi.com>
2013-01-26 22:04:06 +0200

    Decreases neckbeard delay

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -4,4 +4,4 @@ function neckbeardFallback() {
   document.getElementsByTagName('html')[0].className += ' neckbeardFallback'
 }
 
-window.setTimeout(neckbeardFallback, 5000);
+window.setTimeout(neckbeardFallback, 3500);
